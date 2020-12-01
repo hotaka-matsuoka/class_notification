a00667960e7634c3f898bcb6fa6a57d4125c168f
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-12-01 21:41:50 +0900

    Create date.py

---
+++ main/date.py
@@ -0,0 +1,10 @@
+import datetime
+import time
+import locale
+locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+
+dt_now = datetime.datetime.now()
+year = dt_now.year
+month = dt_now.month
+day = dt_now.day
+day_of_week = datetime.date(year, month, day).strftime('%a')
